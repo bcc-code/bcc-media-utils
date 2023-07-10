bd756a71b04f7d4d22a644647653dd48582fdf95
Matjaz Debelak <matjaz.debelak@bcc.no>
2023-07-10 12:08:00 +0200

    Added mon

--- go.mod
+++ go.mod
@@ -5,12 +5,16 @@ go 1.19
 require (
 	cloud.google.com/go/bigquery v1.46.0
 	cloud.google.com/go/firestore v1.9.0
+	cloud.google.com/go/monitoring v1.8.0
 	github.com/ansel1/merry/v2 v2.0.0-beta.12
 	github.com/bcc-code/mediabank-bridge v1.1.1
 	github.com/gin-contrib/cors v1.4.0
 	github.com/gin-gonic/gin v1.9.1
+	github.com/golang/protobuf v1.5.2
 	github.com/rs/zerolog v1.26.0
 	github.com/samber/lo v1.38.1
+	google.golang.org/api v0.108.0
+	google.golang.org/protobuf v1.30.0
 )
 
 require (
@@ -31,7 +35,6 @@ require (
 	github.com/go-playground/validator/v10 v10.14.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/flatbuffers v2.0.8+incompatible // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
@@ -67,10 +70,8 @@ require (
 	golang.org/x/time v0.1.0 // indirect
 	golang.org/x/tools v0.6.0 // indirect
 	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
-	google.golang.org/api v0.108.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/genproto v0.0.0-20230123190316-2c411cf9d197 // indirect
 	google.golang.org/grpc v1.53.0 // indirect
-	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ mon/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	monitoring "cloud.google.com/go/monitoring/apiv3"
+	"cloud.google.com/go/monitoring/apiv3/v2/monitoringpb"
+	"github.com/gin-gonic/gin"
+	"github.com/golang/protobuf/ptypes/timestamp"
+	"google.golang.org/api/iterator"
+	"google.golang.org/protobuf/types/known/durationpb"
+)
+
+func readTimeSeriesValue(projectID, metricType string) (map[string]float64, error) {
+	ctx := context.Background()
+	c, err := monitoring.NewMetricClient(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer c.Close()
+	startTime := time.Now().UTC().Add(time.Minute * -3).Unix()
+	endTime := time.Now().UTC().Unix()
+
+	req := &monitoringpb.ListTimeSeriesRequest{
+		Name:   "projects/" + projectID,
+		Filter: fmt.Sprintf("metric.type=\"%s\" resource.labels.service_name=\"api-prod\" ", metricType),
+		Aggregation: &monitoringpb.Aggregation{
+			AlignmentPeriod: &durationpb.Duration{Seconds: 60},
+			GroupByFields:   []string{"labels.response_code_class"},
+		},
+		Interval: &monitoringpb.TimeInterval{
+			StartTime: &timestamp.Timestamp{Seconds: startTime},
+			EndTime:   &timestamp.Timestamp{Seconds: endTime},
+		},
+	}
+	iter := c.ListTimeSeries(ctx, req)
+
+	vals := make(map[string]float64)
+
+	for {
+		resp, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return nil, fmt.Errorf("could not read time series value, %w ", err)
+		}
+		vals[resp.Metric.Labels["response_code"]] = float64(resp.Points[0].GetValue().GetInt64Value()) / 60.0
+	}
+
+	return vals, nil
+}
+
+func main() {
+	projectID := os.Getenv("PROJECT_ID")
+	r := gin.Default()
+	r.GET("/stats", func(c *gin.Context) {
+		if c.Query("key") != os.Getenv("API_KEY") {
+			c.AbortWithStatus(401)
+		}
+
+		data, err := readTimeSeriesValue(projectID, "run.googleapis.com/request_count")
+		if err != nil {
+			panic(err)
+		}
+
+		c.JSON(200, data)
+	})
+}
