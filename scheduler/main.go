3d9b3bba7fece7a867cceae58983ef79241a97ee
Fredrik Vedvik <fredrik@vedvik.tech>
2023-07-13 13:08:12 +0200

    q

--- scheduler/main.go
+++ scheduler/main.go
@@ -1,16 +1,18 @@
 package main
 
 import (
+	"context"
 	"crypto/tls"
 	"fmt"
 	"github.com/bcc-code/bccm-utils/scheduler/cantemo"
-	"github.com/bcc-code/bccm-utils/scheduler/jobs"
 	"github.com/bcc-code/mediabank-bridge/log"
 	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog"
 	"io"
+	"math/rand"
 	"net/http"
 	"os"
+	"time"
 )
 
 func init() {
@@ -22,14 +24,18 @@ func main() {
 
 	client := cantemo.New(os.Getenv("CANTEMO_URL"), os.Getenv("CANTEMO_AUTH_TOKEN"))
 
-	queue := jobs.NewQueue()
+	q := newQueue(5, func(id string) error {
+		time.Sleep(time.Duration(rand.Intn(10)) * time.Second)
+		return nil
+	})
+
+	go q.run(context.Background())
 
 	r := gin.Default()
 	r.GET("jobs", func(ctx *gin.Context) {
-		j, _ := queue.GetJobs()
-		ctx.JSON(200, j)
+		ctx.JSON(200, q)
 	})
-	r.POST("jobs", jobPostHandler(client, queue))
+	r.POST("jobs", jobPostHandler(client, q))
 
 	err := r.Run(fmt.Sprintf(":%s", os.Getenv("PORT")))
 	if err != nil {
@@ -37,7 +43,7 @@ func main() {
 	}
 }
 
-func jobPostHandler(client *cantemo.Client, queue *jobs.Queue) gin.HandlerFunc {
+func jobPostHandler(client *cantemo.Client, queue *queue) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		body := ctx.Request.Body
 		q, _ := io.ReadAll(body)
@@ -48,8 +54,34 @@ func jobPostHandler(client *cantemo.Client, queue *jobs.Queue) gin.HandlerFunc {
 			return
 		}
 
-		iterateThroughIDs(client, ids)
+		for _, id := range ids {
+			queue.queue(id)
+		}
 
 		ctx.JSON(200, ids)
 	}
+}
+
+func getItemIDsFromSearchQuery(client *cantemo.Client, query string) ([]string, error) {
+	res, err := client.Search().Put(query, 1)
+	if err != nil {
+		return nil, err
+	}
+
+	var ids []string
+	for _, r := range res.Results {
+		ids = append(ids, r.VidispineId)
+	}
+
+	for res.Page < res.Pages {
+		res, err = client.Search().Put(query, res.Page+1)
+		if err != nil {
+			log.L.Error().Err(err).Send()
+		}
+		for _, r := range res.Results {
+			ids = append(ids, r.VidispineId)
+		}
+	}
+
+	return ids, nil
 }
